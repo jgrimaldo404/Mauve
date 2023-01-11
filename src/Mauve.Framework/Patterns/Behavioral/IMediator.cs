9661d69aa1813e44535d663f8d589eea5111d0bf
tacosontitan <jkdavis1691@gmail.com>
2023-01-11 17:53:02 -0600

    Updated mediator signatures.

--- src/Mauve.Framework/Patterns/Behavioral/IMediator.cs
+++ src/Mauve.Framework/Patterns/Behavioral/IMediator.cs
@@ -5,7 +5,9 @@ namespace Mauve.Patterns.Behavioral
 {
     public interface IMediator
     {
-        T Send<T>(IRequest<T> request);
-        Task<T> Send<T>(IRequest<T> request, CancellationToken cancellationToken);
+        void Send<T>(T request) where T : IRequest;
+        TOut Send<TRequest, TOut>(TRequest request) where TRequest : IRequest<TOut>;
+        Task Send<T>(T request, CancellationToken cancellationToken) where T : IRequest;
+        Task<TOut> Send<TRequest, TOut>(TRequest request, CancellationToken cancellationToken) where TRequest : IRequest<TOut>;
     }
 }
