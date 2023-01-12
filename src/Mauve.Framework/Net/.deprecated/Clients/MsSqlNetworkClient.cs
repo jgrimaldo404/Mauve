5391dac3247801008cdcf7d5acc68b292a9a7e76
tacosontitan <jkdavis1691@gmail.com>
2023-01-12 14:05:34 -0600

    Removed regions from MsSqlNetworkClient.

--- src/Mauve.Framework/Net/.deprecated/Clients/MsSqlNetworkClient.cs
+++ src/Mauve.Framework/Net/.deprecated/Clients/MsSqlNetworkClient.cs
@@ -6,17 +6,11 @@ namespace Mauve.Net
 {
     public class MsSqlNetworkClient : INetworkClient
     {
-
-        #region Fields
-
         private readonly string _server;
         private readonly string _database;
         private readonly string _username;
         private readonly string _password;
         private readonly TimeSpan _timeout;
-
-        #endregion
-
         public MsSqlNetworkClient() { }
         public MsSqlNetworkClient(string server, string database)
         {
@@ -35,9 +29,6 @@ namespace Mauve.Net
         public MsSqlNetworkClient(string server, string database, string username, string password, TimeSpan timeout) :
             this(server, database, username, password) =>
             _timeout = timeout;
-
-        #region Public Methods
-
         public void Send(INetworkRequest request) => throw new NotImplementedException();
         public T Send<T>(INetworkRequest request) => throw new NotImplementedException();
         public void Send<T>(INetworkRequest<T> request) => throw new NotImplementedException();
@@ -46,8 +37,5 @@ namespace Mauve.Net
         public Task<T> SendAsync<T>(INetworkRequest request, CancellationToken cancellationToken) => throw new NotImplementedException();
         public Task SendAsync<T>(INetworkRequest<T> request, CancellationToken cancellationToken) => throw new NotImplementedException();
         public Task<TOut> SendAsync<TIn, TOut>(INetworkRequest<TIn> request, CancellationToken cancellationToken) => throw new NotImplementedException();
-
-        #endregion
-
     }
 }
