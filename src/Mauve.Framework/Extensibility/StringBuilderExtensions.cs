c4cc198e777920374e95d6f8d6997e087302a01f
tacosontitan <jkdavis1691@gmail.com>
2023-01-12 19:56:41 -0600

    Added StringBuilder.AppendIf extension method.

---
+++ src/Mauve.Framework/Extensibility/StringBuilderExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Text;
+
+namespace Mauve.Extensibility
+{
+    public static class StringBuilderExtensions
+    {
+        public static StringBuilder AppendIf(this StringBuilder builder, string input, bool condition) =>
+            condition
+            ? builder.Append(input)
+            : builder;
+    }
+}
